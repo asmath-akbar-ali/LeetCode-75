5734cf7f0c4a7399cf6b0d5efdfaba49bc751bc4
Asmath Akbar Ali <144229409+asmath-akbar-ali@users.noreply.github.com>
2025-02-19 05:55:18 +0530

    Update 5. Reverse Vowels of a String.java

--- 1. Array or String/5. Reverse Vowels of a String.java
+++ 1. Array or String/5. Reverse Vowels of a String.java
@@ -1,17 +1,12 @@
 // Example 1:
-
 // Input: s = "IceCreAm"
-
 // Output: "AceCreIm"
 
 // Explanation:
-
 // The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
 
 // Example 2:
-
 // Input: s = "leetcode"
-
 // Output: "leotcede"
 
 class Solution {
