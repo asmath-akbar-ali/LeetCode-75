f67f15b30782d0d8cf8dac7388d4a8039aa194f2
Asmath Akbar Ali <144229409+asmath-akbar-ali@users.noreply.github.com>
2025-02-19 05:53:37 +0530

    Update 2. Greatest Common Divisor of Strings.java

--- 1. Array or String/2. Greatest Common Divisor of Strings.java
+++ 1. Array or String/2. Greatest Common Divisor of Strings.java
@@ -1,3 +1,15 @@
+// Example 1:
+// Input: str1 = "ABCABC", str2 = "ABC"
+// Output: "ABC"
+
+// Example 2:
+// Input: str1 = "ABABAB", str2 = "ABAB"
+// Output: "AB"
+
+// Example 3:
+// Input: str1 = "LEET", str2 = "CODE"
+// Output: ""
+
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
         int s1=str1.length(), s2=str2.length();
